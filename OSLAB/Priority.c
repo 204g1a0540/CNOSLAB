fd388c1c2c688a839a3fa442e706ee30417c536b
204g1a0540 <94514799+204g1a0540@users.noreply.github.com>
2022-09-06 12:30:12 +0530

    B.Indu Created  Priority.c

---
+++ OSLAB/Priority.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.Indu
+   Roll No: 204G1A0540
+   Title of the Experiment: CPU Scheduling Using Priority Algorithm
+   Date of Creation: 6-09-2022
+   Date of Execution: 6-09-2022
+*/
