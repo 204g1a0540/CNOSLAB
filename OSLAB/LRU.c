357036af29a5ac399b05e212a965511127a6bff8
204g1a0540 <94514799+204g1a0540@users.noreply.github.com>
2022-11-01 12:40:21 +0530

    B Indu created LRU.c

---
+++ OSLAB/LRU.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.indu
+   Roll No: 204G1A0540
+   Title of the Experiment: replacement pages Using LRU Algorithm
+   Date of Creation: 1-11-2022
+   Date of Execution: 1-11-2022
+*/
