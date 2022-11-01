b517db356524f2cd75977061d22ab5dcc09d1f22
204g1a0540 <94514799+204g1a0540@users.noreply.github.com>
2022-11-01 12:46:58 +0530

    B Indu completed optimal.c with c source code

--- OSLAB/priority.c
+++
@@ -1,7 +0,0 @@
- 
-/* Name Of the Candidate:B.Indu
-   Roll No: 204G1A0540
-   Title of the Experiment: replacement pages Using Priority Algorithm
-   Date of Creation: 1-11-2022
-   Date of Execution: 1-11-2022
-*/
