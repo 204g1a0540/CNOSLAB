e59ac5534a118afcd0b6171ed072ff974759390b
204g1a0540 <94514799+204g1a0540@users.noreply.github.com>
2022-11-01 12:35:28 +0530

    B Indu Created FIFO.c

---
+++ OSLAB/FIFO.c
@@ -0,0 +1,7 @@
+ 
+/* Name Of the Candidate:B.Indu
+   Roll No: 204G1A0540
+   Title of the Experiment: replacement pages Using Fifo Algorithm
+   Date of Creation: 1-11-2022
+   Date of Execution: 1-11-2022
+*/
