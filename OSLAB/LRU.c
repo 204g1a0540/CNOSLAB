08537c80084c2ac3e3bb5cae8fe4db1175b5c5b3
204G1A0540 <204G1A0540@srit.ac.in>
2022-11-26 14:41:42 +0530

    My LRU.c was completed

--- OSLAB/LRU.c
+++ OSLAB/LRU.c
@@ -5,14 +5,12 @@
    Date of Execution: 1-11-2022
 */
 #include<stdio.h>
-#include<conio.h>
 int fr[3];
 void main()
 {
 void display();
 int p[12]={2,3,2,1,5,2,4,5,3,2,5,2},i,j,fs[3];
 int index,k,l,flag1=0,flag2=0,pf=0,frsize=3;
-clrscr();
 for(i=0;i<3;i++)
 {
 fr[i]=-1;
@@ -61,7 +59,6 @@ pf++;
 display();
 }
 printf("\n no of page faults :%d",pf+frsize);
-getch();
 }
 
 void display()
