c24bb9cc93e25bfd0196359718321f4fe4421524
204G1A0540 <204G1A0540@srit.ac.in>
2022-11-29 12:56:53 +0530

    Dlp.c completed

--- OSLAB/Dlp.c
+++ OSLAB/Dlp.c
@@ -5,14 +5,12 @@
    Date of Execution: 29-11-2022
 */
 #include<stdio.h>
-#include<conio.h>
 
 void main()
 {
 char job[10][10];
 int time[10],avail,tem[10],temp[10]; int safe[10];
 int ind=1,i,j,q,n,t;
-clrscr();
 printf("Enter no of jobs: ");
 scanf("%d",&n);
 for(i=0;i<n;i++)
@@ -58,5 +56,5 @@ printf("No safe sequence\n");
 printf("Safe sequence is:");
 for(i=1;i<ind; i++)
 printf("%s %d\n",job[safe[i]],time[safe[i]]);
-getch();
+
 }
